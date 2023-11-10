26643e75bf1dbcd654981d5726eaf2449594eda9
YVic95 <vika.yevlentieva@gmail.com>
2023-11-10 16:17:41 +0200

    foreach, if and style bindings

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ function viewModel() {
     this.myText = ko.observable ('Hello world!');
     this.myHtml = ko.observable('<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>');
     this.items = ko.observableArray();
+    const obj1 = {a: 1, b: 2};
+    this.obj = ko.observable(obj1);
+    this.isActive = ko.observable(false);
 
     setTimeout(() => {
         this.myText('Hello country!');
@@ -9,7 +12,12 @@ function viewModel() {
 
     this.onClick = function(viewModel, event) {
         this.items.push('Item');
-    }
+        this.obj({...obj1, b: ++obj1.b});
+        this.isActive(!this.isActive());
+    }  
 }
 
 ko.applyBindings(new viewModel());
+
+
+
