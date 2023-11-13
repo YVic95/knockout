ab2ae80bd56b6cbd48f38101ed25ebfd4d8bfc89
YVic95 <vika.yevlentieva@gmail.com>
2023-11-13 17:56:09 +0200

    Example of subscription to ko observable usage

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 function viewModel() {
-    ko.bindingHandlers.afterRender = {
-        init: function(element, valueAccessor, allBindings, viewModel) {
-            // This will be called when the binding is first applied to an element
-            console.log('valueAccessor', valueAccessor);
-            valueAccessor().apply(viewModel, [element,viewModel]);
+    this.primitiveData = ko.observable('Default string');
+    this.dataArray = ko.observableArray(['item 1', 'item 2']);
+    const data = {a: 1, b: 2};
+    this.dataObject = ko.observable(data);
 
-        }
-    };
-    this.onElementRendered = function(element, viewModel) {
-        console.log(element);
-        console.log(viewModel);
-    }  
+    this.primitiveData.subscribe(value => console.log('string', value));
+    this.dataArray.subscribe(value => console.log('array', value));
+    this.dataObject.subscribe(value => console.log('object', value));
+
+    console.group('Default values');
+    console.log(this.primitiveData());
+    console.log(this.dataArray());
+    console.log(this.dataObject());
+    console.groupEnd();
+
+    this.onUpdate = function() {
+        this.primitiveData('Updated string');
+        this.dataArray.push('item 3');
+        this.dataObject({...data, c: ++data.b});
+    }
 }
 const model = new viewModel(); 
 ko.applyBindings(model);
