dd55553e2fca1bea3b0c704f965a658b1341e163
YVic95 <vika.yevlentieva@gmail.com>
2023-11-14 18:15:57 +0200

    KO computed observables

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 function viewModel() {
-    this.primitiveData = ko.observable('Default string');
-    this.dataArray = ko.observableArray(['item 1', 'item 2']);
-    const data = {a: 1, b: 2};
-    this.dataObject = ko.observable(data);
+   this.firstName = ko.observable('');
+   this.lastName = ko.observable('');
+   this.cardHolderName = ko.observable('');
+   
+   // Use pureComputed() when computed observable simply calculates 
+   // and returns a value based on some observable dependencies
 
-    this.primitiveData.subscribe(value => console.log('string', value));
-    this.dataArray.subscribe(value => console.log('array', value));
-    this.dataObject.subscribe(value => console.log('object', value));
-
-    console.group('Default values');
-    console.log(this.primitiveData());
-    console.log(this.dataArray());
-    console.log(this.dataObject());
-    console.groupEnd();
-
-    this.onUpdate = function() {
-        this.primitiveData('Updated string');
-        this.dataArray.push('item 3');
-        this.dataObject({...data, c: ++data.b});
-    }
+   this.customerName = ko.computed(function() {
+    const fullName = `${this.firstName()} ${this.lastName()}`;
+    this.cardHolderName(fullName.toUpperCase());
+    return fullName;
+    //return this.firstName() + " " + this.lastName();
+    
+   }, this); 
 }
 const model = new viewModel(); 
 ko.applyBindings(model);
