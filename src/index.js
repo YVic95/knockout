14fa7cb9172a0c8b6a8a14303f6e9445959baf21
YVic95 <vika.yevlentieva@gmail.com>
2023-11-16 11:11:48 +0200

    Example of how context changes  when using foreach binding

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 function viewModel() {
-   this.firstName = ko.observable('');
-   this.lastName = ko.observable('');
-   this.cardHolderName = ko.observable('');
    
-   // Use pureComputed() when computed observable simply calculates 
-   // and returns a value based on some observable dependencies
-
-   this.customerName = ko.computed(function() {
-    const fullName = `${this.firstName()} ${this.lastName()}`;
-    this.cardHolderName(fullName.toUpperCase());
-    return fullName;
-    //return this.firstName() + " " + this.lastName();
+    this.collections = ko.observableArray([['item 1', ' item 2'], ['item1']]);
     
-   }, this); 
+    this.onClick = function() {
+        console.log('click');
+    }
 }
 const model = new viewModel(); 
 ko.applyBindings(model);
 window.model = model;
-
-
-
